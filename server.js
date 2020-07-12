3e9dad2d83dcecc2a74184d610c7d9bc8009515b
Glitch (hello-express) <none>
2020-07-12 22:37:02 +0000

    🚩🍱 Checkpoint ./views/index.pug:1164060/5394 ./server.js:1164060/1291 ./public/stylesheets/style.css:1164060/991 ./public/scripts/script.js:1164060/1310

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ var path = require("path");
 app.use(express.static("public"));
 
 // View engine setup
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", {
-    title: "Resource Bank"
+app.get("/", function(req, res) {
+  res.render("index", { 
+    title: "Sarthak Mohanty"
   });
 });
-
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
