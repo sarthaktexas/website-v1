239d5b98028d380dcd708d8ccb0e6262f8c2d38e
Glitch (sarthaks-website) <none>
2020-07-13 20:04:47 +0000

    🍂🙇 Checkpoint ./views/index.pug:1164060/166 ./public/scripts/script.js:1164060/81114 ./public/scripts/typewriter.js:1164060/39812

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,3 +6,19 @@ console.log(`%c ________________________________________
             (__)\\       )\\/\\
                 ||----w |
                 ||     ||`, "font-family:monospace");
+
+var tw = document.getElementById('tw');
+
+var typewriter = new Typewriter(tw, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
