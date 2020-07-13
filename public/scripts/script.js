c9f011686eb4b8ab786b1896991d593cbb1e0480
Glitch (sarthaks-website) <none>
2020-07-13 18:42:26 +0000

    🌗👖 Checkpoint ./public/stylesheets/style.css:1164060/3604 ./public/scripts/script.js:1164060/367

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+console.log(`%c ________________________________________
+< mooooooooooooooooooooooooooooooooooooo >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`, "font-family:monospace");
+console.danger("HEY! If you see this, please hire me!");
