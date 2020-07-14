a9bb52fc2afea6e91539f8167f0306056494c719
Glitch (sarthaks-website) <none>
2020-07-14 07:52:21 +0000

    🕋⛈ Checkpoint ./server.js:1164060/2421 ./package.json:1164060/981 ./.glitch-assets:1164060/409

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var fs = require("fs");
 var path = require("path");
 
 app.use(express.static("public"));
@@ -9,9 +10,23 @@ app.use(express.static("public"));
 app.set("view engine", "pug");
 
 app.get("/", function(req, res) {
-  res.render("index", { 
+  res.render("index", {
     title: "Sarthak Mohanty"
   });
+});
+
+app.get("/brand", function(req, res, next) {
+  var stream = fs.createReadStream("https://sarthakmohanty.s3.amazonaws.com/Brand+Colors.pdf");
+  var filename = "Brand Colors.pdf";
+  // Be careful of special characters
+
+  filename = encodeURIComponent(filename);
+  // Ideally this should strip them
+
+  res.setHeader("Content-disposition", 'inline; filename="' + filename + '"');
+  res.setHeader("Content-type", "application/pdf");
+
+  stream.pipe(res);
 });
 
 // listen for requests :)
