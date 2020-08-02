1c1c7093307cc773390b8301fcb7500272162b9d
Glitch (sarthaks-website) <none>
2020-08-02 02:21:30 +0000

    👷💆 Checkpoint ./server.js:1164060/4 ./views/location.pug:1164060/33

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/contact", function(req, res) {
     title: "Contact Me"
   });
 });
-app.get("/location", async function(req, res) {
+app.get("/locate", async function(req, res) {
   var devices = await myCloud.FindMe.get();
   var longitude = JSON.stringify(devices.content[2].location.longitude);
   var latitude = JSON.stringify(devices.content[2].location.latitude);
