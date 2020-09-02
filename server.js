3614c8d0e4697a9b6af738270b179aa0529d0603
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-09-02 16:08:53 -0500

    🗑 clean up codebase

--- server.js
+++ server.js
@@ -10,45 +10,21 @@ var session = {};
 var username = process.env.ICLOUD_USERNAME;
 var password = process.env.ICLOUD_PASSWORD;
 
-var myCloud = new iCloud(session, username, password);
-myCloud.on("ready", function() {
-  const needsSecurityCode = myCloud.twoFactorAuthenticationIsRequired;
-
-  if (needsSecurityCode) {
-    console.error(
-      "Two Factor Authentication is required. Type in your security code!"
-    );
-  } else {
-    console.log("Everything okay. Go on!");
-  }
-});
-
-myCloud.login(username, password, function(err) {
-  if (err) {
-  }
-  myCloud.securityCode = process.env.ICLOUD2FACODE;
-  console.log("You logged in successfully!");
-});
-
-myCloud.on("sessionUpdate", function() {
-  myCloud.saveSession();
-});
-
 app.use(express.static("public"));
 
 app.set("view engine", "pug");
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index", {
     title: "Sarthak Mohanty"
   });
 });
 
-app.get("/contact", function(req, res) {
+app.get("/contact", function (req, res) {
   res.redirect("https://sarthakmohanty.me/contact");
 });
 
-app.get("/tasks", async function(req, res) {
+app.get("/tasks", async function (req, res) {
   try {
     // get todoist api
     await todoist.sync();
@@ -57,7 +33,7 @@ app.get("/tasks", async function(req, res) {
     console.log(items);
     // List of to-do's
     var taskList = [];
-    items.forEach(function(element) {
+    items.forEach(function (element) {
       if (element.due) {
         taskList.push({
           task: element.content,
@@ -77,8 +53,6 @@ app.get("/tasks", async function(req, res) {
         });
       }
     });
-    // Debug stuff
-    //console.log(JSON.stringify(items));
     res.render("tasks", {
       title: "Tasks",
       tasks: taskList
@@ -88,14 +62,29 @@ app.get("/tasks", async function(req, res) {
   }
 });
 
-app.get("/locate", async function(req, res) {
+app.get("/locate", async function (req, res) {
   try {
-    // Log in again just to make sure
-    myCloud.login(username, password, function(err) {
-      if (err) {
+    var myCloud = new iCloud(session, username, password);
+    myCloud.on("ready", function () {
+      const needsSecurityCode = myCloud.twoFactorAuthenticationIsRequired;
+
+      if (needsSecurityCode) {
+        console.error(
+          "Two Factor Authentication is required. Type in your security code!"
+        );
+      } else {
+        console.log("Everything okay. Go on!");
       }
+    });
+
+    myCloud.login(username, password, function (err) {
+      if (err) {}
       myCloud.securityCode = process.env.ICLOUD2FACODE;
-      console.log("You logged in again successfully!");
+      console.log("You logged in successfully!");
+    });
+
+    myCloud.on("sessionUpdate", function () {
+      myCloud.saveSession();
     });
     // Get iCloud location
     var devices = await myCloud.FindMe.get();
@@ -104,10 +93,10 @@ app.get("/locate", async function(req, res) {
     var latitude = JSON.stringify(devices.content[2].location.latitude);
     res.render("location", {
       title: "Location",
-      //longitude: longitude,
-      //latitude: latitude
-      longitude: "-98.628067",
-      latitude: "29.645479"
+      longitude: longitude,
+      latitude: latitude
+      //longitude: "-98.628067",
+      //latitude: "29.645479"
     });
   } catch (error) {
     console.log(error);
@@ -119,32 +108,26 @@ app.get("/locate", async function(req, res) {
   }
 });
 
-app.get("/score", function(req, res) {
+app.get("/score", function (req, res) {
   res.send("This page is currently under maintenance.");
   // res.render("score", {
   //   title: "Sarthak Mohanty - AP Scores"
   // });
 });
 
-app.get("/about", function(req, res) {
+app.get("/about", function (req, res) {
   res.redirect(
     "https://www.notion.so/Directory-Of-Sarthak-8feff445502242ce9cf106795364b360"
   );
 });
 
-app.get("/about", function(req, res) {
+app.get("/about", function (req, res) {
   res.redirect(
     "https://www.notion.so/Directory-Of-Sarthak-8feff445502242ce9cf106795364b360"
   );
 });
 
-const data = require("./info.json");
-
-app.get("/api/*", function(req, res) {
-  res.send("seems like you don't know how to use this api. ask Sarthak.");
-});
-
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.status(404).render("error");
 });
 
